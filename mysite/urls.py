1916ea7f4cb04c7ab3589fccfae79ec8e84aa27d
aryan7081 <haryan458@gmail.com>
2022-03-12 16:11:50 +0530

    url change

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('add/',views.add,name = "add"),
     path('',views.main,name = "main"),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
